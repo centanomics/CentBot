dfbafa8d2714246a21479612f85e893c2055cf3a
semyers189 <semyers@fullsail.edu>
2020-08-08 16:42:01 -0400

    added help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,7 +1,14 @@
+const Discord = require('discord.js');
+
 module.exports = {
   name: 'help',
   description: 'Shows a list of commands',
   execute: async (message, args) => {
-    message.channel.send('list of commands');
+    let helpCommands = new Discord.MessageEmbed().setTitle('List of Commands');
+    message.client.commands.map((command) => {
+      const commandName = command.name[0].toUpperCase() + command.name.slice(1);
+      helpCommands.addField(`${commandName}:`, command.description);
+    });
+    message.channel.send({ embed: helpCommands });
   },
 };
