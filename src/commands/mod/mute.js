10d3a88a11f7ef6901f3c16392c06d368168e233
semyers189 <semyers@fullsail.edu>
2020-08-08 16:10:35 -0400

    added permission check to mute and unmute

--- src/commands/mod/mute.js
+++ src/commands/mod/mute.js
@@ -2,7 +2,11 @@ module.exports = {
   name: 'mute',
   description: 'mutes people',
   execute: async (message, args) => {
-    const user = message.mentions.members.first();
-    user.roles.add('601979552956416011');
+    if (!message.member.hasPermission('VIEW_AUDIT_LOG')) {
+      message.reply("You don't have permission to use this command!");
+    } else {
+      const user = message.mentions.members.first();
+      user.roles.add('601979552956416011');
+    }
   },
 };
--- src/commands/mod/unmute.js
+++ src/commands/mod/unmute.js
@@ -2,7 +2,11 @@ module.exports = {
   name: 'unmute',
   description: 'unmutes people',
   execute: (message, args) => {
-    const user = message.mentions.members.first();
-    user.roles.remove('601979552956416011');
+    if (!message.member.hasPermission('VIEW_AUDIT_LOG')) {
+      message.reply("You don't have permission to use this command!");
+    } else {
+      const user = message.mentions.members.first();
+      user.roles.remove('601979552956416011');
+    }
   },
 };
