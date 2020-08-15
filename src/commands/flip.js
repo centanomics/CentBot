68c592a061d32ee73b2ecc0a659d091e2d5f6fd1
semyers189 <semyers@fullsail.edu>
2020-08-14 21:56:57 -0400

    added a flip command

---
+++ src/commands/flip.js
@@ -0,0 +1,29 @@
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
+// @command     flip
+// @desc        flips a set amount of coins.
+// @access      all
+module.exports = {
+  name: 'flip',
+  description: 'Flips a set amount of clothes.',
+  execute: (message, args) => {
+    const coins = parseInt(args[0]);
+    if (coins >= 1967) {
+      message.channel.send('You can only flip up to 1966 coins');
+      return;
+    }
+    let results = '';
+    for (let i = 0; i < coins; i++) {
+      const flip = getRandomInt(2);
+      if (flip === 0) {
+        results += 'H';
+      } else if (flip === 1) {
+        results += 'T';
+      }
+    }
+
+    message.channel.send(`Results of the ${coins} coin flip(s): ${results}`);
+  },
+};
