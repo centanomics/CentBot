f312387fcc80914d00f448a6800688ef0c48b2df
semyers189 <semyers@fullsail.edu>
2020-08-25 21:53:26 -0400

    hides mod commands for nonmods; shows nonmod commands for mods

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,12 +11,15 @@ module.exports = {
   execute: async (message, args) => {
     const isMod = isAuthorized(message, false);
     let helpCommands = new Discord.MessageEmbed().setTitle('List of Commands');
-    message.client.commands.map((command) => {
-      const commandName = command.name[0].toUpperCase() + command.name.slice(1);
-      if (command.mod === isMod) {
+    message.client.commands
+      .filter((command) => {
+        return !isMod ? command.mod === false : command;
+      })
+      .map((command) => {
+        const commandName =
+          command.name[0].toUpperCase() + command.name.slice(1);
         helpCommands.addField(`${commandName}:`, command.description);
-      }
-    });
+      });
 
     message.channel.send({ embed: helpCommands });
   },
