7cbacae6629591f35a54580e99725e6241dbd7d3
semyers189 <semyers@fullsail.edu>
2020-08-28 01:47:19 -0400

    added ability to translate en > de and vice versa to specific channels

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3432;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
 
 /* START BOT CODE */
 
--- src/events/message/message.js
+++ src/events/message/message.js
@@ -1,7 +1,18 @@
+const { toDE, toEN } = require('../../utils/translator');
 const prefix = process.env.PREFIX;
 
 module.exports = (client, message) => {
   // checks if the message had the prefix or from itself
+  if (!message.content.startsWith(prefix) && !message.author.bot) {
+    //english
+    if (message.channel.id === '521497382572130304') {
+      toDE(message);
+      //german
+    } else if (message.channel.id === '748757584005038201') {
+      toEN(message);
+    }
+    return;
+  }
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   // gets the command file and runs the execute function
