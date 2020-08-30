e3f789c5123a85011397b883d76b6ceaf6e3c46e
semyers189 <semyers@fullsail.edu>
2020-08-29 22:46:33 -0400

    setup public assets

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
