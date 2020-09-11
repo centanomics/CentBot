f194843d40a68cc712dcd7581ffd791b4aafedb2
semyers189 <semyers@fullsail.edu>
2020-09-10 22:35:06 -0400

    setup switch for arguments

--- src/commands/clubs.js
+++ src/commands/clubs.js
@@ -7,5 +7,19 @@ module.exports = {
   mod: false,
   execute: (message, args) => {
     message.channel.send('starting to manage clubs');
+    switch (args[0]) {
+      case 'create':
+        message.channel.send('create');
+        return;
+      case 'join':
+        message.channel.send('join');
+        return;
+      case 'leave':
+        message.channel.send('delete');
+        return;
+      case 'delete':
+        message.channel.send('leave');
+        return;
+    }
   },
 };
