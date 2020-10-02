0d8aaa1a25b34dab87e2b084305a763965063e6f
semyers189 <semyers@fullsail.edu>
2020-10-01 23:22:08 -0400

    added verification for defaultrole command

--- src/commands/mod/defaultrole.js
+++ src/commands/mod/defaultrole.js
@@ -9,7 +9,13 @@ module.exports = {
   mod: true,
   execute: async (message, args) => {
     try {
+      const defaultRole = await Defaults.findOne({ guildId: message.guild.id });
+
       const role = message.mentions.roles.first();
+      if (!role) {
+        throw { message: 'You have to tag a proper role!' };
+      }
+
       const newDefault = new Defaults({
         guildId: message.guild.id,
         roleId: role.id,
@@ -18,6 +24,10 @@ module.exports = {
       message.channel.send(
         `${args[0]} set as the default role for this server`
       );
+
+      if (defaultRole) {
+        await defaultRole.remove();
+      }
     } catch (err) {
       console.log(err);
       message.channel.send(err.message);
