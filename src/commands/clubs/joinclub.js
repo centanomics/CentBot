b00e062dc2bc6ff87d57a2d75886fb1daa817eb9
semyers189 <semyers@fullsail.edu>
2020-10-05 22:06:21 -0400

    fixed validation bug with joinclub command

--- src/commands/clubs/joinclub.js
+++ src/commands/clubs/joinclub.js
@@ -9,14 +9,14 @@ module.exports = {
   mod: false,
   execute: async (message, args) => {
     try {
-      // gets all clubs for the guild
-      const club = await Clubs.find({
+      // gets all the club for the guild by name
+      const club = await Clubs.findOne({
         name: args[0],
         guildId: message.guild.id,
       });
-
-      //shows user list of clubs they can join if they don't enter a valid club name
-      if (club.length === 0) {
+      console.log(club.roleId);
+      //shows user list of clubs in the server if they don't enter a valid club name
+      if (!club) {
         const clubs = await Clubs.find({ guildId: message.guild.id });
         let rtnString = 'List of clubs you can join:\n';
         for (let i = 0; i < clubs.length; i++) {
@@ -26,19 +26,16 @@ module.exports = {
       }
 
       // checks to see if a user is already in a club
-      message.member.roles.cache.map(async (role) => {
-        try {
-          const cloob = await Clubs.findOne({ roleId: role.id });
-          if (cloob) {
-            throw { message: 'You are already in this club!' };
-          }
-        } catch (err) {
-          throw { message: err.message };
+      const userRoles = Array.from(message.member.roles.cache.keys());
+      for (let i = 0; i < userRoles.length; i++) {
+        if (userRoles[i] === club.roleId) {
+          throw { message: 'You already joined this club!' };
         }
-      });
+      }
 
-      message.member.roles.add(club[0].roleId);
-      message.channel.send(`You joined the ${club[0].name} club`);
+      // give the user the appropriate role and notifies them
+      message.member.roles.add(club.roleId);
+      message.channel.send(`You joined the ${club.name} club`);
     } catch (err) {
       console.log(err.message);
       message.channel.send(err.message);
