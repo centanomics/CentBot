d8389d6a8be61dbbb8e9c4f4679faed63290e96b
semyers189 <semyers@fullsail.edu>
2021-02-25 10:22:30 -0500

    added running commands in the message file

--- twitch/events/message.js
+++ twitch/events/message.js
@@ -1,6 +1,21 @@
-module.exports = (client, channel, userstate, msg, self) => {
-  if (self) return;
+const prefix = '$';
 
-  console.log(channel, '\n\n', userstate, '\n\n', msg, '\n\n', self)
-  client.say(channel, `@${userstate['display-name']}, hello there`)
+module.exports = (client, channel, userstate, msg, self) => {
+  if (self || !msg.startsWith(prefix)) return;
+
+  // gets the command file and runs the execute function
+  const args = msg.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if (client.commands.get(command)) {
+    if (client.commands.get(command).mod) {
+      if (isAuthorized(msg, true)) {
+        client.commands.get(command).execute(msg, args);
+      }
+    } else {
+      client.commands.get(command).execute(msg, args);
+    }
+  } else {
+    // if the command doesn't exist, notify the user
+    client.say(channel, `${command} command does not exist`);
+  }
 };
