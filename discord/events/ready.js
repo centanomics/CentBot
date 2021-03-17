29bb03a634c49583e32bb8c7825b2af2e7dc97fd
semyers189 <semyers@fullsail.edu>
2021-03-16 20:10:42 -0400

    added a reminder every 24hr

--- discord/events/ready.js
+++ discord/events/ready.js
@@ -1,4 +1,8 @@
 // sends a log once the client is ready
-module.exports = (client) => {
+module.exports = async (client) => {
   console.log('Penny is online!');
+  const channel = await client.channels.fetch('521497382572130304');
+  let reminder = setInterval(() => {
+    channel.send('WATCH THE FREAKING HAIKYUU EPISODE ALREADY');
+  }, 86400000)
 };
