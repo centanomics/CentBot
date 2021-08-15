5c69b826da0440c0a398737acba2e51aa92eab4a
semyers189 <semyers@fullsail.edu>
2021-08-14 23:34:38 -0400

    added a helper for the random command when no arguments are given

--- discord/commands/random.js
+++ discord/commands/random.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js');
+
 const removeQuotes = (args) => {
   args[0] = args[0].slice(1);
   args[args.length - 1] = args[args.length - 1].slice(
@@ -16,12 +18,24 @@ module.exports = {
   delay: 0,
   mod: false,
   execute: (message, args) => {
+    if (args.length === 0) {
+      const randomHelper = new Discord.MessageEmbed();
+      randomHelper.setTitle('Random Help.');
+      randomHelper.addField(
+        '$bets create "[item-a]" ... "[item-z]"',
+        'Chooses a random option from the options given. No limit to how many options there can be.'
+      );
+      message.channel.send({ embed: randomHelper });
+      return;
+    }
     let moreArgs = args
       .join(' ')
       .replace(/([“”])/g, '"')
-      .split('" "')
+      .split('" "');
     moreArgs = removeQuotes(moreArgs);
-    
-    message.channel.send(moreArgs[Math.floor(Math.random() * Math.floor(moreArgs.length))]);
+
+    message.channel.send(
+      moreArgs[Math.floor(Math.random() * Math.floor(moreArgs.length))]
+    );
   },
 };
